80a0664bb83ab027283cbd8118bcf851c95e9c9a
Shweta <shweta@joshsoftware.com>
2015-03-07 16:26:09 +0530

    Minor changes in recipe view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307151919) do
+ActiveRecord::Schema.define(version: 20150307151920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,9 @@ ActiveRecord::Schema.define(version: 20150307151919) do
     t.integer  "content_id"
     t.integer  "recipe_id"
     t.string   "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "recipes", force: :cascade do |t|
